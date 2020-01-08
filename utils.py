981bcbdbef1e973b9cbdccb30865a108a3eef90a
Zac Todd <zactodd0@gmail.com>
2020-01-09 10:36:32 +1300

    Update utils.py

--- utils.py
+++ utils.py
@@ -212,3 +212,9 @@ def reciprocal_mod(x, m):
         a, b = b, a - x // y * b
         x, y = y, x % y
     return a % m
+
+
+def is_perfect_square(n):
+    root = math.sqrt(n)
+    return int(root + 0.5) ** 2 == n
+
