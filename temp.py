08c1d97b7d63f6137a5ff2168ed1c656982f896c
Zac Todd <zactodd0@gmail.com>
2020-01-13 18:38:27 +1300

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-import os
-
-f_str = "answer{:03d}.py"
-
-answer_files = "Answers"
-for f in os.listdir(answer_files):
-    _, n = f.split("question")
-    os.rename("Answers/" + f, f_str.format(int(n[:-3])))
