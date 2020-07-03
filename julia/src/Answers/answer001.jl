7175e0c50ffa8b60dc3f12ec6bade8a01f98855b
Zac Todd <zactodd0@gmail.com>
2020-07-03 22:14:08 +1200

    Create answer001.jl

---
+++ julia/src/Answers/answer001.jl
@@ -0,0 +1,26 @@
+#=
+answer001:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-03
+
+Problem 1:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+=#
+include("utils.jl")
+
+
+divideGauss(n, d) = d * Utils.gauss(div(n, d))
+
+
+function answer()
+    n = 999
+    return divideGauss(n, 5) + divideGauss(n, 3) - divideGauss(n, 15)
+end
+
+
+# Output
+println("Answer is: ", answer())
