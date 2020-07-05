bb6e860e884d39d94bd116c355d9adc934edc50c
Zac Todd <zactodd0@gmail.com>
2020-07-06 00:09:45 +1200

    answer 021

--- julia/src/Answers/answer021.jl
+++ julia/src/Answers/answer021.jl
@@ -1,4 +1,9 @@
-"""
+#=
+answer021:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-05
+
 Problem 21:
 Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
 If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
@@ -6,22 +11,25 @@ If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and e
 For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
-"""
+
+=#
 
 
-def divisor_sums(n):
-    ds = [0] * n
-    for i in range(1, n):
-        for j in range(i * 2, n, i):
-            ds[j] += i
+function divisor_sums(n)
+    ds = zeros(n)
+    for i in 1:n, j in (i * 2):i:n
+        ds[j] += i
+    end
     return ds
+end
 
 
-def answer():
+function answer()
     n = 10000
     ds = divisor_sums(n)
-    return sum(i for i, s in enumerate(ds) if s != i and s < n and ds[s] == i)
+    return sum([i for (i, s) in enumerate(ds) if s != i && s < n && ds[Int(s)] == i])
+end
 
 
-if __name__ == '__main__':
-    print("Answer is:", answer())
+# Output
+println("Answer is: ", answer())
