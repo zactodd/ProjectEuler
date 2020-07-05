23e935700b958ecd7ce0473d05d3ae439e6eec07
Zac Todd <zactodd0@gmail.com>
2020-07-06 00:10:06 +1200

    answer 131

--- julia/src/Answers/answer131.jl
+++ julia/src/Answers/answer131.jl
@@ -1,4 +1,9 @@
-"""
+#=
+answer131:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-05
+
 Problem 131:
 There are some prime values, p, for which there exists a positive integer, n, such that the expression n3 + n2p is a perfect cube.
 
@@ -7,15 +12,15 @@ For example, when p = 19, 83 + 82×19 = 123.
 What is perhaps most surprising is that for each prime with this property the value of n is unique, and there are only four such primes below one-hundred.
 
 How many primes below one million have this remarkable property?
-"""
-from python.utils import fast_primes
-import math
 
+=#
 
-def answer():
-    m = 10 ** 6
-    return len(set(3 * x ** 2 + 3 * x + 1 for x in range(int(math.sqrt(m / 3)))).intersection(fast_primes(m)))
+include("../utils.jl")
 
+answer(m=BigInt(1000000)) = length(
+    intersect(Set(Utils.sieves(m)), Set(map(x -> 3 * x ^ 2 + 3 * x + 1, 1:floor(sqrt(m / 3)))))
+)
 
-if __name__ == '__main__':
-    print("Answer is:", answer())
+# Output
+println("Answer is: ", answer())
+
