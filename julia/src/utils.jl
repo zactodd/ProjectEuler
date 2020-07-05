13714f5c500ff5e244fe67cf948ef5ec3027bdde
Zac Todd <zactodd0@gmail.com>
2020-07-05 17:09:38 +1200

    Update utils.jl

--- julia/src/utils.jl
+++ julia/src/utils.jl
@@ -6,7 +6,7 @@ utils:
 =#
 module Utils
 
-export PHI, SQRT5, gauss, nth_fib, is_prime, is_palindrome
+export PHI, SQRT5, gauss, nth_fib, is_prime, is_palindrome, sieves, reciprocal_mod
 
 
 SQRT5 = sqrt(5)
@@ -36,5 +36,32 @@ end
 
 
 # nth_prime(n) = IterTools.nth(primes, n)
+
+is_coprime(p, i) = !any(x -> i % x == 0, p)
+
+function sieves(n)
+       is_prime = ones(Bool, n)
+       is_prime[1] = false
+       for i in 2:BigInt(round(sqrt(n)))
+           if is_prime[i]
+              for j in (i * i):i:n
+                is_prime[j] = false
+              end
+           end
+      end
+     return filter(x -> is_prime[x], 1:n)
+end
+
+
+function reciprocal_mod(x, m)
+    y = x
+    x = m
+    a, b = 0, 1
+    while y != 0
+        a, b = b, a - div(x, y) * b
+        x, y = y, x % y
+    end
+    return a % m
+end
 
 end
