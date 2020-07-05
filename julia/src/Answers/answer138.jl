022e74de303b2db757d84cf8d17f179d78a9bb44
Zac Todd <zactodd0@gmail.com>
2020-07-06 00:10:57 +1200

    answer 138

--- julia/src/Answers/answer138.jl
+++ julia/src/Answers/answer138.jl
@@ -1,5 +1,9 @@
-"""
-Problem 138:
+#=
+answer138:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-05
+
 Consider the isosceles triangle with base length, b=16, and legs, L=17.
 
 
@@ -8,15 +12,14 @@ By using the Pythagorean theorem it can be seen that the height of the triangle,
 With b=272 and L=305, we get h=273, which is one more than the base length, and this is the second smallest isosceles triangle with the property that h=b±1.
 
 Find ∑L for the twelve smallest isosceles triangles for which h=b±1 and b, L are positive integers.
-"""
+=#
 
-from python.utils import fast_fib
-
-
-def answer():
-    ns = 12
-    return sum(fast_fib(6 * n + 3) // 2 for n in range(1, ns + 1))
+include("../utils.jl")
 
 
-if __name__ == '__main__':
-    print("Answer is:", answer())
+answer(ns=12) = sum(map(n -> div(Utils.nth_fib(6 * n + 3), 2), 1:ns))
+
+
+
+# Output
+println("Answer is: ", answer())
