ba34b11867d59d821c830d6735130fecc03cd9d9
Zac Todd <zactodd0@gmail.com>
2020-07-06 19:34:35 +1200

    Create answer024.jl

---
+++ julia/src/Answers/answer024.jl
@@ -0,0 +1,23 @@
+#=
+answer024:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-06
+
+Problem 24;
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+=#
+using Combinatorics, IterTools
+
+
+
+answer(n::BigInt=1000000) = IterTools.nth(Combinatorics.permutations(0:9, 9), n)
+
+
+# Output
+println("Answer is: ", answer())
