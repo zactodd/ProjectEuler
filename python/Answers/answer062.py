1571ef242904215b2615a8c733c118864aabccf6
Zac Todd <zactodd0@gmail.com>
2020-07-08 19:19:44 +1200

    Update answer062.py

--- python/Answers/answer062.py
+++ python/Answers/answer062.py
@@ -4,22 +4,20 @@ The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104
 
 Find the smallest cube for which exactly five permutations of its digits are cube.
 """
+import math
+from collections import defaultdict
 
 
 def answer():
-    digits, min_cube = "", float('-Inf')
+    digits, min_cube = "", float('Inf')
     n, d = 100, 5
-
-    cubes = {}
-    while min_cube == float('Inf') or len(digits) <= len(str(min_cube)):
+    cubes = defaultdict(list)
+    while len(digits) <= math.log10(min_cube):
         c = n ** 3
         digits = "".join(sorted(str(c)))
-        if digits in cubes:
-            cubes[digits].append(c)
-            if len(cubes[digits]) == d and min_cube > (candidate := cubes[digits][0]):
-                min_cube = candidate
-        else:
-            cubes[digits] = [c]
+        cubes[digits].append(c)
+        if len(cubes[digits]) == d and min_cube > (candidate := cubes[digits][0]):
+            min_cube = candidate
         n += 1
     return min_cube
 
