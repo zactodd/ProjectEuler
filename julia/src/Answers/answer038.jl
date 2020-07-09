9632ff3f3be48c47174c0d4a965347b3ebc24991
Zac Todd <zactodd0@gmail.com>
2020-07-09 20:50:14 +1200

    answer 038

--- julia/src/Answers/answer038.jl
+++ julia/src/Answers/answer038.jl
@@ -1,5 +1,8 @@
-"""
-Problem 38:
+#=
+answer038:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-09
 
 Take the number 192 and multiply it by each of 1, 2, and 3:
 
@@ -11,14 +14,17 @@ By concatenating each product we get the 1 to 9 pandigital, 192384576. We will c
 The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
 
 What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
-"""
+
+=#
 
 
-def answer():
-    return next((
-        int(p) for n in range(9999, 9000, -1) if sorted(p := str(n) + str(n * 2)) == list("123456789")
-    ), None)
+function answer()
+    for n in 9999:-1:9000
+        p = sort([digits(2 * n)..., digits(n)...])
+        sort(p) == Array(1:9) && return string(n) * string(n * 2)
+    end
+end
 
 
-if __name__ == '__main__':
-    print("Answer is:", answer())
+# Output
+println("Answer is: ", answer())
