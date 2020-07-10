e6227a772cb1bf713d31337d4fc659568ab025df
Zac Todd <zactodd0@gmail.com>
2020-07-10 15:39:14 +1200

    Create answer121.jl

---
+++ julia/src/Answers/answer121.jl
@@ -0,0 +1,31 @@
+#=
+answer121:
+- Julia version: 1.4.2
+- Author: Zac
+- Date: 2020-07-10
+
+Problem 121:
+A bag contains one red disc and one blue disc. In a game of chance a player takes a disc at random and its colour is noted. After each turn the disc is returned to the bag, an extra red disc is added, and another disc is taken at random.
+
+The player pays £1 to play and wins if they have taken more blue discs than red discs at the end of the game.
+
+If the game is played for four turns, the probability of a player winning is exactly 11/120, and so the maximum prize fund the banker should allocate for winning in this game would be £10 before they would expect to incur a loss. Note that any payout will be a whole number of pounds and also includes the original £1 paid to play the game, so in the example given the player actually wins £9.
+
+Find the maximum prize fund that should be allocated to a single game in which fifteen turns are played.
+
+=#
+
+
+function answer()
+    n = 15
+    r = div(n - 1, 2)
+    p::Array{BigInt} = vcat([1], zeros(r))
+    for k in 0:n
+        p[r + 1:-1:2] += 0:n * p[r:-1:1]
+    end
+    return div(factorial(n + 1), sum(p))
+end
+
+
+# Output
+println("Answer is: ", answer())
