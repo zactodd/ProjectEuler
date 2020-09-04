51acb7342af48318e36509601f89d3c265fc4344
Zac Todd <zactodd0@gmail.com>
2020-09-04 15:49:04 +1200

    Update answer052.py

--- python/Answers/answer052.py
+++ python/Answers/answer052.py
@@ -9,9 +9,8 @@ from python.utils import is_perm
 
 
 def answer():
-    # More effect with doing check inside 13: nested for
-    candidates = [int("".join(p)) for i in range(6, 9) for p in permutations("0123456789", i) if p[0] != "0"]
-    return next((c for c in candidates if all(is_perm(c, c * i) for i in range(2, 7))), None)
+    candidates = [int("".join(p)) for i in range(6, 9) for p in permutations("1234567890", i)]
+    return next(c for c in candidates if all(is_perm(c, c * i) for i in range(2, 7)))
 
 
 if __name__ == '__main__':
