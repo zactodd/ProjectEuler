52094abc9409b94c4f4a04a438fa9c1810f63dc8
Zac Todd <zactodd0@gmail.com>
2020-09-10 21:27:38 +1200

    Update answer142.py

--- python/Answers/answer142.py
+++ python/Answers/answer142.py
@@ -9,7 +9,7 @@ from python.utils import is_perfect_square
 def answer():
     return next((c + e + a) // 2 for i, a in map(lambda x: (x, x ** 2), count(4))
                 for j, c in filter(lambda x: is_perfect_square(a - x[1]), map(lambda x: (x, x ** 2), range(3, i)))
-                for k in range(1 if j % 2 == 1 else 2, j, 2)
+                for k in range(1 if j % 2 else 2, j, 2)
                 if (e := a - k ** 2) > 0 and (b := c - e) > 0 and is_perfect_square(e) and is_perfect_square(b))
 
 
