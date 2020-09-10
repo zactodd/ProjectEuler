61acfdb1eae86bf23530dc256d93c39e2fc6975d
Zac Todd <zactodd0@gmail.com>
2020-09-10 21:33:58 +1200

    Update answer120.jl

--- julia/src/Answers/answer120.jl
+++ julia/src/Answers/answer120.jl
@@ -3,6 +3,13 @@ answer120:
 - Julia version: 1.4.2
 - Author: Zac
 - Date: 2020-09-10
+
+Problem 120:
+Let r be the remainder when (a−1)n + (a+1)n is divided by a2.
+
+For example, if a = 7 and n = 3, then r = 42: 63 + 83 = 728 ≡ 42 mod 49. And as n varies, so too will r, but for a = 7 it turns out that rmax = 42.
+
+For 3 ≤ a ≤ 1000, find ∑ rmax.
 =#
 
 
