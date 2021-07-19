7d38be8049b9181d064fe5ed09700e2b8eac1f1c
Zac Todd <zactodd0@gmail.com>
2021-07-19 14:03:21 +1200

    Update answer179.py

--- python/Answers/answer179.py
+++ python/Answers/answer179.py
@@ -3,14 +3,12 @@ Problem 179:
 
 Find the number of integers 1 < n < 107, for which n and n + 1 have the same number of positive divisors. For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
 """
+from collections import Counter
 
 
 def answer(n=10 ** 7):
-    ds = [2] * n
-    for i in range(2, (len(ds) + 1) // 2):
-        for j in range(i * 2, len(ds), i):
-            ds[j] += 1
-    return sum(i == j for i, j in zip(ds[2:], ds[3:]))
+    ds = Counter(j for i in range(2, (n + 1) // 2) for j in range(i * 2, n, i))
+    return sum(ds[i] == ds[i + 1] for i in range(2, n))
 
 
 if __name__ == "__main__":
